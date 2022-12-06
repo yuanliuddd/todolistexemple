49ac55f2ad0ed343e5c79de6ffe85e7f319811d7
YUAN LIU <yuanliu0120@gmail.com>
2022-12-06 17:11:52 +0100

    AddGuest Type

--- src/component/AddGuest.tsx
+++ src/component/AddGuest.tsx
@@ -7,7 +7,28 @@ interface IAddGuest {
 }
 
 export default function AddGuest({ setPeople, people }: IAddGuest) {
-  const [input, setInput] = useState();
+  const [input, setInput] = useState({
+    name: "",
+    age: 0,
+    note: "",
+    img: "",
+  });
 
-  return <div>  </div>;
+  const onSubmitHandler = (): void => {
+    setPeople(input);
+  };
+
+  const onChangeHandler = (): void => {};
+
+  return (
+    <div>
+      <form onSubmit={onSubmitHandler}>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button type="submit"></button>
+      </form>
+    </div>
+  );
 }
